cbcc57f3aaf954ab01400cc715a76858d9e5aeb9
doblepapi <jasonbnick@gmial.com>
2020-07-23 20:04:18 +0200

    add event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
 let tweetButton = document.querySelector('#tweet-submit');
 let tweetInput = document.querySelector('#tweet-input');
+
+tweetButton.addEventListener('submit', getTweet);
+
+function getTweet(event){
+    console.log(event.target);
+}
