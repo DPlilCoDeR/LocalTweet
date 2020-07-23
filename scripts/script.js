d7f13cd772eff429c1ddeb33350d0c4d2ef2c5c5
doblepapi <jasonbnick@gmial.com>
2020-07-23 20:14:47 +0200

    eventListener get value of input

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let tweetButton = document.querySelector('#tweet-submit');
 let tweetInput = document.querySelector('#tweet-input');
 
-tweetButton.addEventListener('submit', getTweet);
+tweetButton.addEventListener('click', saveTweet);
 
-function getTweet(event){
-    console.log(event.target);
+function saveTweet(event){
+    let tweet = tweetInput.value;
+    console.log('bien', tweet);
+    event.preventDefault();
 }
