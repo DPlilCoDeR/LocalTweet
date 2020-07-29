33a9f8b73b2ad732ea67dfac2a6e829425f90b4c
doblepapi <jasonbnick@gmial.com>
2020-07-29 17:01:21 +0200

    provisional refactor getTweetsLocalStorage()

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,19 @@ function eventListener() {
     form.addEventListener('submit', createTweetNodeItemList);
     document.addEventListener('DOMContentLoaded', getTweetsLocalStorage);
 }
+
 function getTweetsLocalStorage() {
     for (let index = 0; index < myStorage.length; index++) {
         let storageItem = myStorage.getItem(myStorage.key(index));
+        let itemList = document.createElement('li');
+        let deleteButton = document.createElement('button');
+        deleteButton.style.color = 'red'
+        deleteButton.innerText = 'X';
+
+        deleteButton.onclick = deleteTweet;
+        itemList.appendChild(deleteButton);
+        itemList.append(storageItem);
+        list.append(itemList);
     }
 }
 
@@ -20,7 +30,7 @@ function createTweetNodeItemList(event) {
 
     let itemList = document.createElement('li');
     let deleteButton = document.createElement('button');
-    deleteButton.style.backgroundColor = 'red'
+    deleteButton.style.color = 'red'
     deleteButton.innerText = 'X';
 
     deleteButton.onclick = deleteTweet;
